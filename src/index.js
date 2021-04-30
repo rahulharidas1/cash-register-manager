b28016d0e3ff6553b4ca46a0ba38d1cc54b084f0
Rahul Haridas <48543567+rahulharidas1@users.noreply.github.com>
2021-04-30 18:48:43 +0530

    added table to display result

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ var nextBtn = document.getElementById("next");
 var amountReceived = document.getElementById("amountReceived");
 var calcBtn = document.getElementById("calculate");
 var secondBox = document.getElementById("secondBox");
+var tableResult = document.getElementsByClassName("result");
 const getNextElement = () => {
   secondBox.style.display = "block";
   nextBtn.style.display = "none";
 };
-const calculateChange = (billAmount, receivedAmount) => {
-  var denominations = [2000, 500, 100, 20, 10, 5, 1];
-  var currentBalance = Number(receivedAmount) - Number(billAmount);
-  var changeToBeReturned = [];
+var denominations = [2000, 500, 100, 20, 10, 5, 1];
+var changeToBeReturned = [];
+const calculateChange = () => {
+  var billAmount = inputTxt.value;
+  var receivedAmount = amountReceived.value;
+
+  var currentBalance = receivedAmount - billAmount;
+
   for (let i = 0; i < denominations.length; i++) {
-    if (denominations[i] < currentBalance) {
+    if (denominations[i] <= currentBalance) {
       var currentDenomination = denominations[i];
       var notes = Math.floor(currentBalance / currentDenomination);
       changeToBeReturned.push(notes);
@@ -23,10 +28,9 @@ const calculateChange = (billAmount, receivedAmount) => {
   }
 
   console.log(changeToBeReturned);
+  // console.log(tableResult.item(1).innerHTML);
+  changeToBeReturned = [];
   // return changeToBeReturned;
 };
 nextBtn.addEventListener("click", getNextElement);
-calcBtn.addEventListener(
-  "click",
-  calculateChange(inputTxt.value, amountReceived.value)
-);
+calcBtn.addEventListener("click", calculateChange);
