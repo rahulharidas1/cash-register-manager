d2b6aa42abdd94267d62a6ed81abe003ae7ed436
Rahul Haridas <48543567+rahulharidas1@users.noreply.github.com>
2021-05-01 12:37:21 +0530

    feat: Added input validation

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ var calcBtn = document.getElementById("calculate");
 var secondBox = document.getElementById("secondBox");
 var tableResult = document.getElementsByClassName("result");
 var table = document.querySelector(".result-table");
+var errorMsg = document.querySelector(".errormsg");
+var calcError = document.querySelector(".calc-error");
+
 table.style.setProperty("display", "none");
 const getNextElement = () => {
-  secondBox.style.display = "block";
-  nextBtn.style.display = "none";
+  if (/^\d+$/.test(inputTxt.value)) {
+    if (inputTxt.value === 0) {
+      errorMsg.innerHTML = "Bill Amount cannot be zero!";
+    } else {
+      secondBox.style.display = "block";
+      nextBtn.style.display = "none";
+      errorMsg.style.setProperty("display", "none");
+    }
+  } else {
+    errorMsg.innerHTML = "Please enter a number";
+  }
 };
 var denominations = [2000, 500, 100, 20, 10, 5, 1];
 var changeToBeReturned = [];
@@ -17,24 +29,35 @@ const calculateChange = () => {
   var receivedAmount = amountReceived.value;
 
   var currentBalance = receivedAmount - billAmount;
-
-  for (let i = 0; i < denominations.length; i++) {
-    if (denominations[i] <= currentBalance) {
-      var currentDenomination = denominations[i];
-      var notes = Math.floor(currentBalance / currentDenomination);
-      changeToBeReturned.push(notes);
-      currentBalance = currentBalance % currentDenomination;
+  if (/^\d+$/.test(billAmount) && /^\d+$/.test(receivedAmount)) {
+    if (receivedAmount < billAmount) {
+      calcError.innerHTML = "Amount received is less than the total bill!";
     } else {
-      changeToBeReturned.push(0);
+      calcError.style.setProperty("display", "none");
+      for (let i = 0; i < denominations.length; i++) {
+        if (denominations[i] <= currentBalance) {
+          var currentDenomination = denominations[i];
+          var notes = Math.floor(currentBalance / currentDenomination);
+          changeToBeReturned.push(notes);
+          currentBalance = currentBalance % currentDenomination;
+        } else {
+          changeToBeReturned.push(0);
+        }
+      }
+
+      console.log(changeToBeReturned);
+      table.style.removeProperty("display");
+      changeToBeReturned.map((item, index) => {
+        return (tableResult.item(index).innerHTML = item);
+      });
     }
+  } else {
+    console.log("input a number!");
+    table.style.setProperty("display", "none");
+    calcError.innerHTML = "Please enter valid input";
   }
 
-  console.log(changeToBeReturned);
-  table.style.removeProperty("display");
-  changeToBeReturned.map((item, index) => {
-    return (tableResult.item(index).innerHTML = item);
-  });
-  // console.log(tableResult.item(1).innerHTML);
+  console.log(/^\d+$/.test(billAmount));
   changeToBeReturned = [];
   // return changeToBeReturned;
 };
